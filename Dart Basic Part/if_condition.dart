b335d7fe0adef22a7c46ba6bd026b9fbc26e658f
tuhin <hossaintuhin6666@gmail.com>
2024-10-17 23:31:08 +0530

    if condition

--- Dart Basic Part/if_condition.dart
+++ Dart Basic Part/if_condition.dart
@@ -1,8 +1,18 @@
+/// akta decision make korte (if conditon) use kora hoi
+/*
+if conditon syntax
+data type- variable name = value;
+if (condition ){
+print(body)
+}
+ */
+
 void main(){
   int number = 10;
   if (number>1){
     print('this number is positive');
   }
+
   var num =9;
   if(num<10){
     print('this num is negative');
